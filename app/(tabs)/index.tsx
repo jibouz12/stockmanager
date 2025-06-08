6c614a04741a6bdb35c2f396a892d92fb3e52d0b
jibouz12 <75794029+jibouz12@users.noreply.github.com>
2025-06-09 00:35:21 +0200

    Updated index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -193,7 +193,7 @@ export default function AllStockScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#FFF',
+    backgroundColor: '#AAA',
   },
   loadingContainer: {
     flex: 1,
