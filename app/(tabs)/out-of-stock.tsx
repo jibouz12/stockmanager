8b318eb6b86809b0f80ea3cff86a72b70d51f6db
jibouz12 <75794029+jibouz12@users.noreply.github.com>
2025-06-09 00:48:17 +0200

    Updated out-of-stock.tsx

--- app/(tabs)/out-of-stock.tsx
+++ app/(tabs)/out-of-stock.tsx
@@ -146,7 +146,7 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     borderBottomColor: '#E5E7EB',
     marginBottom: 8,
-    marginTop: 24,
+    marginTop: 28,
   },
   titleContainer: {
     flexDirection: 'row',
