e3dac2f40dd1ffd779d5480d5f2070dcc2f82fc4
jibouz12 <75794029+jibouz12@users.noreply.github.com>
2025-06-09 00:47:02 +0200

    Updated expiring.tsx

--- app/(tabs)/expiring.tsx
+++ app/(tabs)/expiring.tsx
@@ -177,6 +177,7 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     borderBottomColor: '#E5E7EB',
     marginBottom: 8,
+    marginTop: 18,
   },
   titleContainer: {
     flexDirection: 'row',
